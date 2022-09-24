9bd09b7ae39d7563038266addc8e4a7301a7e7b9
gnzlplcs <palaciosgm@outlook.com>
2022-09-24 01:15:42 -0500

    create Home snippet (HTML structure)

--- home.php
+++ home.php
@@ -1,1 +1,54 @@
-<h1>Title</h1>
+<section class="hero">
+  <h1>Welcome to PHP Motors!</h1>
+  <div class="description--float">
+    <h5>DMC Delorean</h5>
+    <p>3 Cup holders</p>
+    <p>Superman doors</p>
+    <p>Fuddy dice!</p>
+  </div>
+  <div class="img-container">
+    <img src='./images/delorean.jpg' alt="Delorean model outline" />
+  </div>
+  <div class="btnImg-container">
+    <img src='./images/site/own_today.png' alt="Own Today button" />
+  </div>
+</section>
+<section class="reviews">
+  <h2>DMC Delorean Reviews</h2>
+  <ul>
+    <li>"So fast its almost like traveling in time." (4/5)</li>
+    <li>"Coolest ride on the road." (4/5)</li>
+    <li>"I'm feeling Marty McFly!" (5/5)</li>
+    <li>"The most futuristic ride of our day." (4.5/5)</li>
+    <li>"80's livin and I love it!" (5/5)</li>
+  </ul>
+</section>
+<section class="upgrades">
+  <h2>Delorean Upgrades</h2>
+  <div class="wrapper-upgrades">
+    <div class="upgrade-container">
+      <div class="upgradeImg-container">
+        <img src="./images/upgrades/flux-cap.png">
+      </div>
+      <a href="#">Flux Capacitor</a>
+    </div>
+    <div class="upgrade-container">
+      <div class="upgradeImg-container">
+        <img src="./images/upgrades/flame.jpg">
+      </div>
+      <a href="#">Flame Decals</a>
+    </div>
+    <div class="upgrade-container">
+      <div class="upgradeImg-container">
+        <img src="./images/upgrades/bumper_sticker.jpg">
+      </div>
+      <a href="#">Bumper Stickers</a>
+    </div>
+    <div class="upgrade-container">
+      <div class="upgradeImg-container">
+        <img src="./images/upgrades/hub-cap.jpg">
+      </div>
+      <a href="#">Hub Caps</a>
+    </div>
+  </div>
+</section>
