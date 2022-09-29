d162f916c19cc148673242768ba7c743c4a6481a
gnzlplcs <palaciosgm@outlook.com>
2022-09-29 16:25:58 -0500

    modify home.php & tempalte.php + change the main view from template.php to home.php, in order to display this last one when phpmotore is executed

--- home.php
+++ home.php
@@ -1,61 +1,89 @@
-<section class="hero">
-  <h1>Welcome to PHP Motors!</h1>
-  <div class="hero__container">
-    <div class="hero__description">
-      <h3>DMC Delorean</h3>
-      <p>3 Cup holders</p>
-      <p>Superman doors</p>
-      <p>Fuzzy dice!</p>
-      <div class="hero__btnContainer--two">
-        <img src='./images/site/own_today.png' alt="Own Today button" />
-      </div>
-    </div>
-    <div class="hero__imgContainer">
-      <img src='./images/delorean.jpg' alt="Delorean model outline" />
-    </div>
-    <div class="hero__btnContainer--one">
-      <img src='./images/site/own_today.png' alt="Own Today button" />
-    </div>
-  </div>
 </section>
+<!DOCTYPE html>
+<html lang="en">
 
-<section class="reviews">
-  <h2>DMC Delorean Reviews</h2>
-  <ul class="reviews__list">
-    <li class="reviews__list--item">"So fast its almost like traveling in time." (4/5)</li>
-    <li class="reviews__list--item">"Coolest ride on the road." (4/5)</li>
-    <li class="reviews__list--item">"I'm feeling Marty McFly!" (5/5)</li>
-    <li class="reviews__list--item">"The most futuristic ride of our day." (4.5/5)</li>
-    <li class="reviews__list--item">"80's livin and I love it!" (5/5)</li>
-  </ul>
-</section>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home | PHP Motors</title>
+  <link rel="stylesheet" href="./css/style.css?v=2" media="screen">
+</head>
 
-<section class="upgrades">
-  <h2>Delorean Upgrades</h2>
-  <div class="upgrades__container">
-    <div class="upgrade">
-      <div class="upgrade__imgContainer">
-        <img src="./images/upgrades/flux-cap.png" alt="Flux cap diagram">
-      </div>
-      <a href="#">Flux Capacitor</a>
-    </div>
-    <div class="upgrade">
-      <div class="upgrade__imgContainer">
-        <img src="./images/upgrades/flame.jpg" alt="Big and orange flame">
-      </div>
-      <a href="#">Flame Decals</a>
-    </div>
-    <div class="upgrade">
-      <div class="upgrade__imgContainer">
-        <img src="./images/upgrades/bumper_sticker.jpg" alt="Bumper sticker">
-      </div>
-      <a href="#">Bumper Stickers</a>
-    </div>
-    <div class="upgrade">
-      <div class="upgrade__imgContainer">
-        <img src="./images/upgrades/hub-cap.jpg" alt="Silver hub cap image">
-      </div>
-      <a href="#">Hub Caps</a>
-    </div>
+<body>
+  <div class="container">
+    <header class="header">
+      <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/header.php'; ?>
+    </header>
+    <nav class="navbar">
+      <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/navigation.php'; ?>
+    </nav>
+    <main class="content">
+      <section class="hero">
+        <h1>Welcome to PHP Motors!</h1>
+        <div class="hero__container">
+          <div class="hero__description">
+            <h3>DMC Delorean</h3>
+            <p>3 Cup holders</p>
+            <p>Superman doors</p>
+            <p>Fuzzy dice!</p>
+            <div class="hero__btnContainer--two">
+              <img src='./images/site/own_today.png' alt="Own Today button" />
+            </div>
+          </div>
+          <div class="hero__imgContainer">
+            <img src='./images/delorean.jpg' alt="Delorean model outline" />
+          </div>
+          <div class="hero__btnContainer--one">
+            <img src='./images/site/own_today.png' alt="Own Today button" />
+          </div>
+        </div>
+      </section>
+
+      <section class="reviews">
+        <h2>DMC Delorean Reviews</h2>
+        <ul class="reviews__list">
+          <li class="reviews__list--item">"So fast its almost like traveling in time." (4/5)</li>
+          <li class="reviews__list--item">"Coolest ride on the road." (4/5)</li>
+          <li class="reviews__list--item">"I'm feeling Marty McFly!" (5/5)</li>
+          <li class="reviews__list--item">"The most futuristic ride of our day." (4.5/5)</li>
+          <li class="reviews__list--item">"80's livin and I love it!" (5/5)</li>
+        </ul>
+      </section>
+
+      <section class="upgrades">
+        <h2>Delorean Upgrades</h2>
+        <div class="upgrades__container">
+          <div class="upgrade">
+            <div class="upgrade__imgContainer">
+              <img src="./images/upgrades/flux-cap.png" alt="Flux cap diagram">
+            </div>
+            <a href="#">Flux Capacitor</a>
+          </div>
+          <div class="upgrade">
+            <div class="upgrade__imgContainer">
+              <img src="./images/upgrades/flame.jpg" alt="Big and orange flame">
+            </div>
+            <a href="#">Flame Decals</a>
+          </div>
+          <div class="upgrade">
+            <div class="upgrade__imgContainer">
+              <img src="./images/upgrades/bumper_sticker.jpg" alt="Bumper sticker">
+            </div>
+            <a href="#">Bumper Stickers</a>
+          </div>
+          <div class="upgrade">
+            <div class="upgrade__imgContainer">
+              <img src="./images/upgrades/hub-cap.jpg" alt="Silver hub cap image">
+            </div>
+            <a href="#">Hub Caps</a>
+          </div>
+        </div>
+    </main>
+    <footer class="footer">
+      <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/footer.php'; ?>
+    </footer>
   </div>
-</section>
+</body>
+
+</html>
--- template.php
+++ template.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Home | PHP Motors</title>
+  <title>Template | PHP Motors</title>
   <link rel="stylesheet" href="./css/style.css?v=2" media="screen">
 </head>
 
@@ -18,7 +18,7 @@
       <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/navigation.php'; ?>
     </nav>
     <main class="content">
-      <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/home.php'; ?>
+      <p>Content goes here</p>
     </main>
     <footer class="footer">
       <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/footer.php'; ?>
