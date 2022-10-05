b892f1ec012d4cb6479d02818fa0ce8a478c3323
gnzlplcs <palaciosgm@outlook.com>
2022-10-05 16:16:52 -0500

    create account folder and its controller, but it doesn't work yet

---
+++ accounts/index.php
@@ -0,0 +1,39 @@
+<?php
+// This is the account controller for the site
+
+// Get the database connection file
+require_once '../library/connections.php';
+
+// Get the PHP Motors model for use as needed
+require_once '../model/main-model.php';
+
+// Get the array of classifications
+$classifications = getClassifications();
+
+// Testing (like JS's console.log)
+// var_dump($classifications);
+//   exit;
+
+// Build a navigation bar using the $classifications array
+$navList = '<ul>';
+$navList .= "<li><a href='/phpmotors/index.php' title='View the PHP Motors home page'>Home</a></li>";
+foreach ($classifications as $classification) {
+  $navList .= "<li><a href='/phpmotors/index.php?action=" . urlencode($classification['classificationName']) . "' title='View our $classification[classificationName] product line'>$classification[classificationName]</a></li>";
+}
+$navList .= '</ul>';
+
+// echo $navList;
+// exit;
+
+
+$action = filter_input(INPUT_POST, 'action');
+
+if ($action == NULL) {
+  $action = filter_input(INPUT_GET, 'action');
+}
+
+switch ($action) {
+  case '':
+    break;
+   default;
+}
