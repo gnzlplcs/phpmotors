734096808983fa11619b86eb30dd2d556fe43c2a
gnzlplcs <palaciosgm@outlook.com>
2022-10-04 19:25:08 -0500

    create getClassification function in main-model.php

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php
 // This is the main controller for the site
 
+// Get the database connection file
+require_once 'library/connections.php';
+
+// Get the PHP Motors model for use as needed
+require_once 'model/main-model.php';
+
+// Get the array of classifications
+$classifications = getClassifications();
+
+// Testing (like JS's console.log)
+// var_dump($classifications);
+//   exit;
+
+
 $action = filter_input(INPUT_POST, 'action');
 
 if ($action == NULL){
