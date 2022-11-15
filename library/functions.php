2b07caeec1905636083f9f9ddb3d068a1dbe4436
gnzlplcs <palaciosgm@outlook.com>
2022-11-14 19:29:47 -0500

    vehicle navigation links completed

--- library/functions.php
+++ library/functions.php
@@ -19,9 +19,9 @@ function checkPassword($clientPassword)
 function showNavList($classifications)
 {
   $navList = '<ul>';
-  $navList .= "<li class='clean-li'><a href='/phpmotors/index.php?action=home' title='View the PHP Motors home page' class='link-onDark'>Home</a></li>";
+  $navList .= "<li class='clean-li'><a href='/phpmotors/' title='View the PHP Motors home page' class='link-onDark'>Home</a></li>";
   foreach ($classifications as $classification) {
-    $navList .= "<li class='clean-li'><a class='link-onDark' href='/phpmotors/index.php?action=" . urlencode($classification['classificationName']) . "' title='View our $classification[classificationName] product line'>$classification[classificationName]</a></li>";
+    $navList .= "<li class='clean-li'><a class='link-onDark' href='/phpmotors/vehicles/?action=classification&classificationName=" . urlencode($classification['classificationName']) . "' title='View our $classification[classificationName] product line'>$classification[classificationName]</a></li>";
   }
   $navList .= '</ul>';
   return $navList;
--- vehicles/index.php
+++ vehicles/index.php
@@ -155,6 +155,17 @@ switch ($action) {
 
     break;
 
+  case 'classification':
+    $classificationName = filter_input(INPUT_GET, 'classificationName', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $vehicles = getVehiclesByClassification($classificationName);
+    if (!count($vehicles)) {
+      $message = "<p class='warning-message'>Sorry, no $classificationName vehicles could be found.</p>";
+    } else {
+      $vehicleDisplay = buildVehiclesDisplay($vehicles);
+    }
+    include '../view/classification.php';
+    break;
+
   default:
     $classificationList = buildClassificationList($classifications);
     include '../view/vehicles-man.php';
